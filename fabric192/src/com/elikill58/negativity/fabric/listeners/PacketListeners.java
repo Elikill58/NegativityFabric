9012fe074f6cddac2a11addccdf34560e4ad8a5d
Elikill58 <arpetzouille@gmail.com>
2022-11-02 16:00:58 +0100

    Use new netty things

--- fabric192/src/com/elikill58/negativity/fabric/listeners/PacketListeners.java
+++ fabric192/src/com/elikill58/negativity/fabric/listeners/PacketListeners.java
@@ -1,15 +1,8 @@
 package com.elikill58.negativity.fabric.listeners;
 
-import java.util.NoSuchElementException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import com.elikill58.negativity.api.entity.Player;
-import com.elikill58.negativity.api.packets.PacketDirection;
-import com.elikill58.negativity.api.packets.nms.channels.netty.NettyDecoderHandler;
-import com.elikill58.negativity.api.packets.nms.channels.netty.NettyEncoderHandler;
+import com.elikill58.negativity.api.packets.nms.channels.netty.NettyPacketListener;
 import com.elikill58.negativity.fabric.impl.entity.FabricEntityManager;
-import com.elikill58.negativity.universal.Adapter;
 import com.elikill58.negativity.universal.utils.ReflectionUtils;
 
 import io.netty.channel.Channel;
@@ -20,13 +13,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-public class PacketListeners {
-
-	private ExecutorService addChannelExecutor = Executors.newSingleThreadExecutor();
-	
-	public ExecutorService getOrCreateAddChannelExecutor() {
-		return addChannelExecutor;
-	}
+public class PacketListeners extends NettyPacketListener {
 
 	public PacketListeners() {
 		ServerPlayConnectionEvents.JOIN.register(this::onPlayerJoin);
@@ -34,50 +21,20 @@ public class PacketListeners {
 	}
 
 	public void onPlayerJoin(ServerPlayNetworkHandler e, PacketSender sender, MinecraftServer srv) {
-		addChannel(e.player);
+		join(FabricEntityManager.getPlayer(e.player));
 	}
 
 	public void onLeave(ServerPlayNetworkHandler e, MinecraftServer srv) {
+		left(FabricEntityManager.getPlayer(e.player));
+	}
+
+	@Override
+	public Channel getChannel(Player p) {
 		try {
-			Channel channel = ReflectionUtils.getFirstWith(e.player.networkHandler.connection, ClientConnection.class, Channel.class);
-			removeChannel(channel, "negativity_decoder");
-			removeChannel(channel, "negativity_encoder");
-		} catch (Exception exc) {
-			exc.printStackTrace();
+			return ReflectionUtils.getFirstWith(((ServerPlayerEntity) p.getDefault()).networkHandler.connection, ClientConnection.class, Channel.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
-	}
-	
-	private void addChannel(ServerPlayerEntity pe) {
-		getOrCreateAddChannelExecutor().execute(() -> {
-			Player p = FabricEntityManager.getPlayer(pe);
-			Channel channel = null;
-			try {
-				channel = ReflectionUtils.getFirstWith(pe.networkHandler.connection, ClientConnection.class, Channel.class);
-				// Managing incoming packet (from player)
-				channel.pipeline().addBefore("decoder", "negativity_decoder", new NettyDecoderHandler(p, PacketDirection.CLIENT_TO_SERVER));
-				
-				// Managing outgoing packet (to the player)
-				channel.pipeline().addAfter("encoder", "negativity_encoder", new NettyEncoderHandler(p));
-			} catch (NoSuchElementException exc) {
-				// appear when the player's channel isn't accessible because of reload.
-				Adapter.getAdapter().getLogger().warn("Please, don't use reload, this can produce some problem. Currently, " + p.getName() + " isn't fully checked because of that. More details: " + exc.getMessage() + " (NoSuchElementException)");
-			} catch (IllegalArgumentException exc) {
-				if(exc.getMessage().contains("Duplicate handler")) {
-					removeChannel(channel, "negativity_decoder");
-					removeChannel(channel, "negativity_encoder");
-					addChannel(pe);
-				} else
-					Adapter.getAdapter().getLogger().error("Error while loading Packet channel. " + exc.getMessage() + ". Please, prefer restart than reload.");
-			} catch (Exception exc) {
-				exc.printStackTrace();
-			}
-		});
-	}
-	
-	private void removeChannel(Channel c, String key) {
-		if(c == null)
-			return;
-		if(c.pipeline().get(key) != null)
-			c.pipeline().remove(key);
 	}
 }
