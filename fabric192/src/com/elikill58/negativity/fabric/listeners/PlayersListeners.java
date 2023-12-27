5c1fe7f6562db69d25a8afb6618057b8fcc82d2b
Elikill58 <arpetzouille@gmail.com>
2023-12-27 19:53:50 +0100

    Fix #6

--- fabric192/src/com/elikill58/negativity/fabric/listeners/PlayersListeners.java
+++ fabric192/src/com/elikill58/negativity/fabric/listeners/PlayersListeners.java
@@ -1,5 +1,7 @@
 package com.elikill58.negativity.fabric.listeners;
 
+import java.net.InetAddress;
+
 import com.elikill58.negativity.api.NegativityPlayer;
 import com.elikill58.negativity.api.events.EventManager;
 import com.elikill58.negativity.api.events.player.LoginEvent;
@@ -12,6 +14,7 @@ import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 public class PlayersListeners {
@@ -23,11 +26,15 @@ public class PlayersListeners {
 	}
 
 	public static void onPreLogin(ServerPlayNetworkHandler e, MinecraftServer srv) {
-		LoginEvent event = new LoginEvent(e.getPlayer().getUuid(), e.getPlayer().getName().getString(), Result.ALLOWED,
-				null, "");
-		EventManager.callEvent(event);
-		if (!event.getLoginResult().equals(Result.ALLOWED))
-			e.disconnect(Text.of(event.getKickMessage()));
+		try {
+			ServerPlayerEntity p = e.getPlayer();
+			LoginEvent event = new LoginEvent(p.getUuid(), p.getName().getString(), Result.ALLOWED, InetAddress.getByName(p.getIp()), "");
+			EventManager.callEvent(event);
+			if (!event.getLoginResult().equals(Result.ALLOWED))
+				e.disconnect(Text.of(event.getKickMessage()));
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
 	}
 
 	public static void onPlayerJoin(ServerPlayNetworkHandler e, PacketSender sender, MinecraftServer srv) {
@@ -38,8 +45,7 @@ public class PlayersListeners {
 	}
 
 	public static void onLeave(ServerPlayNetworkHandler e, MinecraftServer srv) {
-		NegativityPlayer np = NegativityPlayer.getNegativityPlayer(e.getPlayer().getUuid(),
-				() -> new FabricPlayer(e.getPlayer()));
+		NegativityPlayer np = NegativityPlayer.getNegativityPlayer(e.getPlayer().getUuid(), () -> new FabricPlayer(e.getPlayer()));
 		PlayerLeaveEvent event = new PlayerLeaveEvent(np.getPlayer(), np, "");
 		EventManager.callEvent(event);
 		// TODO add again e.setMessage(Text.of(event.getQuitMessage()));
